8fe5a9f15671f1e2d6bca9dc0579b48fa079edf6
Varga, Zoltán <waress@users.noreply.github.com>
2020-04-23 23:35:46 +0200

    Selection example

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ let word = 'cool';
 if (color === 'purple') {
     $('.n').css('background-color', color);
 }
+
+if (number > 100) {
+    $('.e').text("whoah, that's a big number.");
+} else {
+    $('.e').text("just a regular number, please.");
+}
